df687526e921bccb50d590f749762383d2aeab0d
jean <jin.ahn.1027@gmail.com>
2021-07-22 16:04:46 -0400

    change format of categories to make more similar to original

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,22 +3,38 @@ import { Col, Row } from 'react-bootstrap';
 
 const Categories=()=>{
     return (
-        <Row xs={1} md={4}>
+        <Row xs={8} sm={8} lg={8} className="text-center">
             <Col>
+            <img src="https://s3.amazonaws.com/s3.supplyhouse.com/html/img/phone/category/plumbing.jpg" alt="P" width="100%"/>
+            Plumbing >
             </Col>
             <Col>
+            <img src="https://s3.amazonaws.com/s3.supplyhouse.com/html/img/phone/category/heating.jpg" alt="P" width="100%"/>
+            Heating >
             </Col>
             <Col>
+            <img src="https://s3.amazonaws.com/s3.supplyhouse.com/html/img/phone/category/hvac.jpg" alt="P" width="100%"/>
+            HVAC >
             </Col>
             <Col>
+            <img src="https://s3.amazonaws.com/s3.supplyhouse.com/html/img/phone/category/pex.jpg" alt="P" width="100%"/>
+            PEX >
             </Col>
             <Col>
+            <img src="https://s3.amazonaws.com/s3.supplyhouse.com/html/img/phone/category/fittings.jpg" alt="P" width="100%"/>
+            Fittings >
             </Col>
             <Col>
+            <img src="https://s3.amazonaws.com/s3.supplyhouse.com/html/img/phone/category/valves.jpg" alt="P" width="100%"/>
+            Valves >
             </Col>
             <Col>
+            <img src="https://s3.amazonaws.com/s3.supplyhouse.com/html/img/supplyhouse/icons/thermostats.jpg" alt="P" width="100%"/>
+            Thermostats >
             </Col>
             <Col>
+            <img src="https://s3.amazonaws.com/s3.supplyhouse.com/html/img/supplyhouse/icons/electric.jpg" alt="P" width="100%"/>
+            Electrical >
             </Col>
         </Row>
     );
